7385909f74eac2bb2b324595f18e774e2fc50a06
mijx <eperezmi@unal.edu.co>
2025-04-26 15:04:01 -0500

    Add figures to the scene

--- 2025-04-21_taller_jerarquias_transformaciones/threejs/taller2/src/App.jsx
+++ 2025-04-21_taller_jerarquias_transformaciones/threejs/taller2/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from '@react-three/fiber'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Scene() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas>
+      {/* Cámara y luces básicas */}
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      {/* Nodo padre */}
+      <group>
+        {/* Hijo 1 */}
+        <mesh position={[-2, 0, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+
+        {/* Hijo 2 */}
+        <mesh position={[2, 0, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="skyblue" />
+        </mesh>
+      </group>
+    </Canvas>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Scene />
+    </div>
+  )
+}
