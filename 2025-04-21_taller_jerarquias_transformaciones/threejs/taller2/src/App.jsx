cd23ef6fae4e623d2e78ce66e6aa91c33e178c21
mijx <eperezmi@unal.edu.co>
2025-04-26 15:07:34 -0500

    Add slider to control Y-Axis rotation in father node

--- 2025-04-21_taller_jerarquias_transformaciones/threejs/taller2/src/App.jsx
+++ 2025-04-21_taller_jerarquias_transformaciones/threejs/taller2/src/App.jsx
@@ -1,14 +1,23 @@
 import { Canvas } from '@react-three/fiber'
+import { useControls } from 'leva'
+import { useRef } from 'react'
 
 function Scene() {
+  const groupRef = useRef()
+
+  // Slider de leva para rotación en Y
+  const { rotationY } = useControls({
+    rotationY: { value: 0, min: -Math.PI, max: Math.PI, step: 0.01 }
+  })
+
   return (
     <Canvas>
       {/* Cámara y luces básicas */}
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[5, 5, 5]} intensity={2} />
 
       {/* Nodo padre */}
-      <group>
+      <group ref={groupRef} rotation={[0, rotationY, 0]}>
         {/* Hijo 1 */}
         <mesh position={[-2, 0, 0]}>
           <boxGeometry args={[1, 1, 1]} />
